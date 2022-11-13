3504efa82336e9b9084b9198d9067bb2ea1ba089
Mike <devprodct@gmail.com>
2022-11-13 22:32:45 +0200

    [storage] - add records functions

--- actions/storage.js
+++ actions/storage.js
@@ -1,1 +1,31 @@
-import { Low } from 'lowdb';
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+import lodash from 'lodash';
+
+class LowWithLodash extends Low {
+    chain = lodash.chain(this).get('data');
+}
+
+const __dirname     = dirname(fileURLToPath(import.meta.url));
+const file          = join(__dirname, process.env.FILE_PATH)
+const adapter       = new JSONFile(file)
+const db            = new LowWithLodash(adapter)
+
+db.data ||= { records: [] }
+db.read()
+
+export const update = () => db.write()
+
+export const addRecord = async (record) => {
+    db.data.records.push(record)
+    update()
+}
+
+export const addRecords = async (items) => {
+    items.map(item => {
+        addRecord(item)
+    })
+    update();
+}
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import "./config.js";
 import { getJSONMessages } from './actions/discord.js';
+import { addRecords } from "./actions/storage.js";
 
 // const client = require('./orders/order');
 // client.getPosition().then(res => res.result.filter(target => target.data.entry_price !== 0) );
@@ -7,6 +8,9 @@ import { getJSONMessages } from './actions/discord.js';
 const app = async () => {
 
     const jsonMessages = await getJSONMessages();
+
+    // Write record to DB
+    addRecords(jsonMessages);
 }
 
 app();
